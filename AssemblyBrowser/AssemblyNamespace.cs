42ff9ad488086bef1a83d119327fb89a1b789d24
Асус <https://github.com/Ushiramaru/OOP_1.git>
2019-11-27 15:09:39 +0300

    Completed base classes

---
+++ AssemblyBrowser/AssemblyNamespace.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AssemblyBrowser
+{
+    public class AssemblyNamespace
+    {
+        public string Name { get; }
+        public List<AssemblyType> AssemblyTypes { get; }
+
+        public AssemblyNamespace(string name)
+        {
+            Name = name;
+            AssemblyTypes = new List<AssemblyType>();
+        }
+    }
+}
---
+++ AssemblyBrowser/BrowserOfAssembly.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AssemblyBrowser
+{
+    public class BrowserOfAssembly
+    {
+        public BrowsingResult Browse(string assemblyPath)
+        {
+            var assembly = Assembly.LoadFrom(assemblyPath);
+            var types = new List<Type>(assembly.GetTypes());
+
+            var browsingResult = new BrowsingResult();
+            var namespaces = new Dictionary<string, List<Type>>();
+            foreach (var type in types)
+            {
+                var namespaceName = type.Namespace ?? "";
+                if (!namespaces.TryGetValue(namespaceName, out var namespacesTypes))
+                {
+                    namespacesTypes = new List<Type>();
+                    namespaces.Add(namespaceName, namespacesTypes);
+                }
+
+                namespacesTypes.Add(type);
+            }
+
+            foreach (KeyValuePair<string, List<Type>> namespaceType in namespaces)
+            {
+                var assemblyNamespace = new AssemblyNamespace(namespaceType.Key);
+                foreach (var type in namespaceType.Value)
+                {
+                    var assemblyType = new AssemblyType(type);
+                    assemblyNamespace.AssemblyTypes.Add(assemblyType);
+                }
+
+                browsingResult.Namespaces.Add(assemblyNamespace);
+            }
+
+            return browsingResult;
+        }
+    }
+}
